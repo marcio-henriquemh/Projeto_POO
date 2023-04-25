1f8a547e92b2608524bf60662014253fd97d8d11
Márcio Henrique Matos de Freitas <70753463+marcio-henriquemh@users.noreply.github.com>
2023-04-25 16:28:33 -0300

    Add files via upload

--- Projeto_Poo/Urna_Eletronica/src/classes_e_objetos/Periodo_Eleitorial.java
+++ Projeto_Poo/Urna_Eletronica/src/classes_e_objetos/Periodo_Eleitorial.java
@@ -1,5 +1,14 @@
 package classes_e_objetos;
 
+//import classes_e_objetos.Objetos_auxiliares.;;
 public class Periodo_Eleitorial {
+
+    public Periodo_Eleitorial(String nome, String titulo2, boolean vice, int digitos, int eleitos) {
+    }
+    private String eleicao;
+    private String titulo;
+
     
+
+
 }
